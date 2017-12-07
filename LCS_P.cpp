3a83b3d5888a8df5a4efac4311102e2a0e7fe91b
Elia (Chandler) Deppe <cdeppe1@umbc.edu>
2017-12-07 16:42:50 -0500

    Add files via upload

--- LCS_P.cpp
+++ LCS_P.cpp
@@ -17,7 +17,6 @@
 
 //includes
 #include <iostream>
-#include <algorithm>
 #include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,27 +24,31 @@
 #include <string>
 #include <sstream>
 #include <omp.h>
+#include <time.h>
 
+using namespace std;
+
+void create_array(int **array, const int len2, const int i);
 
 //function declaration
 //LCS
 //takes in a dynamic 2d array, 2 strings received from the text files, and their
 //  respective lengths
-long LCS(long **array, std::string s1, std::string s2, const long len1, const long len2);
+int LCS(int **array, string s1, string s2, const int len1, const int len2);
 
-void work_row(long **array, std::string s1, std::string s2, const long len2, const long i);
+void work_row(int **array, string s1, string s2, const int len2, const int i);
 
 //main
 int main(int argc, char *argv[]) {
     //invalid amount of arguments
     if(argc != 3) {
-        std::cout << "please supply two text files\n";
+        cout << "please supply two text files\n";
         return 0;
     }
 
-    std::ifstream file;                     //file
-    std::stringstream buffer1, buffer2;     //string buffers
-    std::string s1, s2;                     //strings
+    ifstream file;                     //file
+    stringstream buffer1, buffer2;     //string buffers
+    string s1, s2;                     //strings
 
     //open the first file, read into the buffer, and then convert to a string
     file.open(argv[1]);
@@ -55,7 +58,7 @@ int main(int argc, char *argv[]) {
         file.close();
     }
     else {  //if file didn't open properly
-        std::cout << "Invalid filename for 1st text file, exiting...";
+        cout << "Invalid filename for 1st text file, exiting...";
         return 0;
     }
 
@@ -67,27 +70,31 @@ int main(int argc, char *argv[]) {
         file.close();
     }
     else {  //if file didn't open properly
-        std::cout << "Invalid filename for 2nd text file, exiting...";
+        cout << "Invalid filename for 2nd text file, exiting...";
         return 0;
     }
 
-    const long len1 = (long)s1.length();        //length of string 1
-    const long len2 = (long)s2.length();        //length of string 2
-    long **array = new long *[(len1 + 1)];      //2D dynamic array of size
-    for(long i = 0; i < (len1 + 1); i++) {      //  (len1 + 1) * (len2 + 1)
-        array[i] = new long[(len2 + 1)];        //the + 1 is for the null row/column
-        for(long j = 0; j < (len2 + 1); j++) {
-            array[i][j] = -1;
+    const int len1 = s1.length();        //length of string 1
+    const int len2 = s2.length();        //length of string 2
+    int **array = new int *[(len1 + 1)];      //2D dynamic array of size
+
+    #pragma omp parallel
+    {
+        #pragma omp single
+        for(int i = 0; i < (len1 + 1); i++) {      //  (len1 + 1) * (len2 + 1)
+            #pragma omp task
+            create_array(array, len2, i);
         }
     }
 
+    double t0 = omp_get_wtime();
     //call and output the length of the LCS
-    std::cout << "Length of LCS is: " << LCS(array, s1, s2, len1, len2) << "\n";
-
-    std::cout << array[4][5] << "\n";
+    cout << "Length of LCS is: " << LCS(array, s1, s2, len1, len2) << "\n";
+    double t = omp_get_wtime() - t0;
+    cout << "Total time: " << t << "\n\n";
 
     //delete the array
-    for(long i = 0; i < (len1 + 1); i++) {
+    for(int i = 0; i < (len1 + 1); i++) {
         delete [] array[i];
     }
     delete [] array;
@@ -95,15 +102,23 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+void create_array(int **array, const int len2, const int i) {
+    array[i] = new int[(len2+1)];
+    for(int j = 0; j < (len2 + 1); j++) {
+        array[i][j] = -1;
+    }
+}
+
 
 //LCS
-long LCS(long **array, std::string s1, std::string s2, const long len1, const long len2) {
-    #pragma omp parallel {
+int LCS(int **array, string s1, string s2, const int len1, const int len2) {
+    #pragma omp parallel
+    {
         #pragma omp single
-        for(long i = 0; i < len1 + 1; i++) {        //two for loops, create this graph by columns
+        for(int i = 0; i < len1 + 1; i++) {        //two for loops, create this graph by columns
             array[i][0] = 0;
             #pragma omp task
-            work_row(array, s1, s2, len2, i)
+            work_row(array, s1, s2, len2, i);
         }
     }
     
@@ -111,21 +126,26 @@ long LCS(long **array, std::string s1, std::string s2, const long len1, const lo
     return array[len1][len2];
 }
 
-void work_row(long **array, std::string s1, std::string s2, const long len2, const long i) {
-    for(long j = 1; j < len2 + 1; j++) {
-        if(array[][] == -1) {
-            j--; //stall
-        }
-        else {
-            if(j == 0) {
-                array[i][j] == 0;
-            }
-            else if(s1[i-1] == s2[j-1]) {
-                array[i][j] = array[i-1][j-1] + 1;
+void work_row(int **array, string s1, string s2, const int len2, const int i) {
+    for(int j = 1; j < len2 + 1; j++) {
+        if(i != 0) {
+            if(array[i-1][j] == -1) {
+                j--; //stall
             }
             else {
-                array[i][j] = std::max(array[i-1][j], array[i][j-1]);
-            }
+                if(j == 0) {
+                    array[i][j] == 0;
+                }
+                else if(s1[i-1] == s2[j-1]) {
+                    array[i][j] = array[i-1][j-1] + 1;
+                }
+                else {
+                    array[i][j] = max(array[i-1][j], array[i][j-1]);
+                }
+           }
         }
+	else {
+	    array[i][j] = 0;
+	}
     }
 }
